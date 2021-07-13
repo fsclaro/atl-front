62af80c1c0eb9572c55904eb9cc3995286ab6db6
Nando <fsclaro@gmail.com>
2021-07-13 16:47:06 -0300

    can soft delete and close tasks

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,7 +1,6 @@
 import { FormEvent, useState } from 'react'
-import { ReactNode } from 'react'
-import { Link, useHistory } from 'react-router-dom'
-
+import { Link } from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast'
 import api from '../api'
 
 import '../styles/tasks.scss'
@@ -25,8 +24,6 @@ export default function Tasks() {
   const token = localStorage.getItem('token')
   const authStr = 'Bearer ' + token
 
-  const history = useHistory()
-
   async function getAllTasks() {
     await api
       .get('/tasks', { headers: { Authorization: authStr } })
@@ -48,7 +45,7 @@ export default function Tasks() {
     event.preventDefault()
 
     if (newTask.trim() === '') {
-      alert('Você não digitou uma nova tarefa.')
+      toast.error('Você não digitou a descrição da tarefa');
       return
     }
 
@@ -56,15 +53,16 @@ export default function Tasks() {
       title: newTask,
       completedAt: false,
       completedUntil: null,
-      assignedTo: userId
+      assignedTo: userId,
+      deletedAt: null,
     }
 
     await api
       .post('/tasks', task, { headers: { Authorization: authStr } })
       .then((response) => {
         if (response.data.task) {
-          alert('Tarefa cadastrada com sucesso!!')
           setNewTask('')
+          toast.success(`Tarefa cadastrada com sucesso`)
           window.location.reload();
         }
       })
@@ -73,6 +71,45 @@ export default function Tasks() {
       })
   }
 
+  async function handleCloseTask(taskId: string) {
+    const task = {
+      completed: true,
+      completedAt: new Date(),
+    }
+
+    await api
+      .put(`/tasks/close/${taskId}`, task, { headers: { Authorization: authStr } })
+      .then((response) => {
+        if (response.data.task) {
+          toast.success('Tarefa concluída com sucesso');
+          setNewTask('')
+          window.location.reload()
+        }
+      })
+      .catch((error) => {
+        console.log({ error: error.message })
+      })
+  }
+
+
+  async function handleDeleteTask(taskId: string) {
+    const task = {
+      deletedAt: new Date(),
+    }
+
+    await api
+      .put(`/tasks/softdelete/${taskId}`, task, { headers: { Authorization: authStr } })
+      .then((response) => {
+        if (response.data.task) {
+          toast.success('Tarefa excluída com sucesso!!')
+          setNewTask('')
+          window.location.reload()
+        }
+      })
+      .catch((error) => {
+        console.log({ error: error.message })
+      })
+  }
 
   return (
     <div id="page-room" onLoad={getAllTasks}>
@@ -112,7 +149,11 @@ export default function Tasks() {
                   <p>{value.title}</p>
                   <footer>
                     <div className="user-info">
-                      <span>Criada em: {new Date(value.createdAt).toLocaleString('pt-BR')}</span>
+                      <span>
+                        Criada por:
+                        <span className="user-name">{value.assignedTo.name}</span> em
+                        <span className="created-at">{new Date(value.createdAt).toLocaleString('pt-BR')}</span>
+                      </span>
                       <span>
                         Status:
                         <span className={value.completed ? 'task-closed' : 'task-opened'}>
@@ -121,15 +162,59 @@ export default function Tasks() {
                       </span>
                     </div>
                     <div>
-                      <button type="button"     aria-label="Encerrar Tarefa"
+                      {!value.completed && (
+                        <button
+                          type="button"
+                          aria-label="Encerrar Tarefa"
+                          title="Encerrar Tarefa"
+                          onClick={() => handleCloseTask(value._id)}
+                        >
+                          <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <circle
+                              cx="12.0003"
+                              cy="11.9998"
+                              r="9.00375"
+                              stroke="#737380"
+                              strokeWidth="1.5"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                            <path
+                              d="M8.44287 12.3391L10.6108 14.507L10.5968 14.493L15.4878 9.60193"
+                              stroke="#737380"
+                              strokeWidth="1.5"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                          </svg>
+                        </button>
+                      )}
+                      <button
+                        type="button"
+                        aria-label="Excluir Tarefa"
+                        title="Excluit Tarefa"
+                        onClick={() => handleDeleteTask(value._id)}
                       >
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path
-                            d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z"
+                            d="M3 5.99988H5H21"
                             stroke="#737380"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          />
+                          <path
+                            d="M8 5.99988V3.99988C8 3.46944 8.21071 2.96074 8.58579 2.58566C8.96086 2.21059 9.46957 1.99988 10 1.99988H14C14.5304 1.99988 15.0391 2.21059 15.4142 2.58566C15.7893 2.96074 16 3.46944 16 3.99988V5.99988M19 5.99988V19.9999C19 20.5303 18.7893 21.039 18.4142 21.4141C18.0391 21.7892 17.5304 21.9999 17 21.9999H7C6.46957 21.9999 5.96086 21.7892 5.58579 21.4141C5.21071 21.039 5 20.5303 5 19.9999V5.99988H19Z"
+                            stroke="#737380"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
                           />
                         </svg>
                       </button>
@@ -142,6 +227,7 @@ export default function Tasks() {
 
         </div>
       </main>
+      <Toaster />
     </div>
   )
 }
